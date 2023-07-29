db9d442e629b12b7687234ff917f5794895bc741
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-29 10:57:06 -0400

    configuring form and created RestaurantStatus type

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,6 +14,12 @@ interface Restaurant {
   dishes?: Dish[]
 }
 
+type RestaurantStatus =
+  | 'Want to Try'
+  | 'Recommended'
+  | 'Do Not Recommend'
+  | 'Must Try'
+
 const restaurantList = ref<Restaurant[]>([])
 const newRestaurant = ref<Restaurant>({})
 const addRestaurant = () => {
@@ -31,10 +37,18 @@ const addRestaurant = () => {
     <pre>{{ newRestaurant }}</pre>
     <!-- create a form that allows users to add a restaurant to list -->
     <form @submit.prevent="addRestaurant">
-      <label for="restaurant-name">Restaurant Name</label>
-      <input id="restaurant-name" v-model="newRestaurant.name" type="text" />
-      <label for="restaurant-address">Restaurant Address</label>
-      <input id="restaurant-name" v-model="newRestaurant.address" />
+      <div>
+        <label for="restaurant-name">Restaurant Name</label>
+        <input id="restaurant-name" v-model="newRestaurant.name" type="text" />
+      </div>
+      <div>
+        <label for="restaurant-address">Restaurant Address</label>
+        <input id="restaurant-address" v-model="newRestaurant.address" />
+      </div>
+      <div>
+        <label for="restaurant-status">Restaurant Status</label>
+        <input id="restaurant-status" v-model="newRestaurant.status" />
+      </div>
       <button type="submit">Add Restaurant</button>
     </form>
     <ul>
