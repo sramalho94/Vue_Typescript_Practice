0e0654752e64f5b8b3b1ea1594f59c2f74c0b4e0
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-29 10:52:11 -0400

    building out form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,46 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+
+// restaurants
+// Name - string
+// Address - string
+// Status - string "Want to try" | "Must try"
+// Dishes - array of Dish objects
+
+interface Restaurant {
+  name?: string
+  address?: string
+  status?: string
+  dishes?: Dish[]
+}
+
+const restaurantList = ref<Restaurant[]>([])
+const newRestaurant = ref<Restaurant>({})
+const addRestaurant = () => {
+  restaurantList.value.push({
+    name: newRestaurant.value.name,
+    address: '',
+    status: '',
+    dishes: []
+  })
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <pre>{{ newRestaurant }}</pre>
+    <!-- create a form that allows users to add a restaurant to list -->
+    <form @submit.prevent="addRestaurant">
+      <label for="restaurant-name">Restaurant Name</label>
+      <input id="restaurant-name" v-model="newRestaurant.name" type="text" />
+      <label for="restaurant-address">Restaurant Address</label>
+      <input id="restaurant-name" v-model="newRestaurant.address" />
+      <button type="submit">Add Restaurant</button>
+    </form>
+    <ul>
+      <li v-for="restaurant in restaurantList" :key="restaurant.name">
+        {{ restaurant }}
+      </li>
+    </ul>
   </main>
 </template>
